4920bcc77eed9dda589e2acb839f2b17c34f82a3
ramon <ramonb2@Hotmail.nl>
2020-01-17 16:29:21 +0100

    added comments to code

--- NodeApi/app.js
+++ NodeApi/app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 
 
+// making sure you won't get any CORS errors.
+
 app.options("/*", function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
--- index.php
+++ index.php
@@ -9,7 +9,11 @@
 
 <?php
 require_once 'vendor/autoload.php';
- 
+ /*
+    To run this you first need to run the node.js server. Make sure Node.js is installed.
+    if you installed this open windows powershell or cmd and navigate to the server directory.
+    Type the command npm start in the terminal.
+ */
 use JsonSchema\Validator;
 use JsonSchema\Constraints\Constraint;
  
@@ -21,6 +25,7 @@ $validator = new Validator; $validator->validate(
 );
  
 if ($validator->isValid()) {
+// if the validation is OK the following script will be loaded to draw a chart from the json data.
   echo "<script src='script.js'></script>";
 } else {
   echo "JSON validation errors:\n";
@@ -32,12 +37,13 @@ if ($validator->isValid()) {
 
 
 $xml= new DOMDocument();
-$xml->load("http://10.232.35.47:3000/AGD/xml"); // Or load if filename required
-if (!$xml->schemaValidate("schema.xsd")) // Or schemaValidateSource if string used.
+$xml->load("http://10.232.35.47:3000/AGD/xml"); // loads the xml from the api
+if (!$xml->schemaValidate("schema.xsd")) // validates it according to the schema
 {
-   echo "test";
+   echo "not correct";
 } else{
-    
+    // if the validation is OK the following script will be called to draw the graph based on the 
+    // xml data.
     echo "<script src='xmlscript.js'></script>";
 }
 
