391d8f451638dd912303ad55771abbe8e57bc6f9
Ramonb2 <Ramon.brakels@student.stenden.com>
2023-06-19 00:22:58 +0200

    Brought the Javascript from 2015 to 2023
    -Javascript update
    -start on modal page for inputting raw json and XML data
    -Added simple bootstrap layout

--- Dataprocessing/index.php
+++ Dataprocessing/index.php
@@ -1,33 +1,101 @@
+<!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    <script src='script.js'></script>
     <script src='xmlscript.js'></script>
+    <style>
+        .header {
+            background-color: #000;
+            color: #fff;
+            padding: 10px;
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .header-button {
+            margin-left: auto;
+            height:50px;
+            font-weight: bold;
+        }
+    </style>
     <script>
-      function executeChart(){
-        xmlChart();
-        console.log('test');
-        jsonChart();
-      }
+        function executeChart() {
+            xmlChart();
+            console.log('test');
+            jsonChart();
+        }
+        function openModal() {
+            $('#myModal').modal('show');
+        }
     </script>
 </head>
 <body>
-    <div>
-    <select id="mySelect" onchange="executeChart()">
-      <option value="EU">Europe</option>
-      <option value="AF">Africa</option>
-      <option value="AS">Asia</option>
-      <option value="OC">Oceania</option>
-      <option value="AN">Antartica</option>
-      <option value="SA">South America</option>
-      <option value="NA">North America</option>
-    </select>
-    <canvas id='myChart' width='400' height='400'></canvas>
-</div>
-<div>
-    <canvas id='myChartxml' width='400' height='400'></canvas>
-</div>
+    <div class="header">
+        <h1>Dataprocessing</h1>
+        <button class="btn btn-primary header-button" onclick="openModal()">Add country data</button>
+    </div>
 
+    <div class="container mt-5">
+        <div class="form-group">
+            <label for="mySelect">Select Region:</label>
+            <select id="mySelect" class="form-control" onchange="executeChart()">
+                <option value="EU">Europe</option>
+                <option value="AF">Africa</option>
+                <option value="AS">Asia</option>
+                <option value="OC">Oceania</option>
+                <option value="AN">Antarctica</option>
+                <option value="SA">South America</option>
+                <option value="NA">North America</option>
+            </select>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <canvas id="myChart" width="800" height="800"></canvas>
+            </div>
+            <div class="col-md-12">
+                <canvas id="myChartxml" width="800" height="800"></canvas>
+            </div>
+        </div>
+    </div>
+     <!-- Modal -->
+     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add country</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                        <label for="exampleFormControlSelect1">Format</label>
+                        <select class="form-control" id="exampleFormControlSelect1">
+                          <option>JSON</option>
+                          <option>XML</option>
+                          
+                        </select>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="input1">input</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <!-- Add more input fields if needed -->
+
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
 require_once 'vendor/autoload.php';
  /*
@@ -85,6 +153,12 @@ if (validate_xml(get_xml_from_url("http://localhost:3000/AGD/EU"))){
 }
 
 ?>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
 </body>
 
 </html>
--- Dataprocessing/xmlscript.js
+++ Dataprocessing/xmlscript.js
@@ -1,85 +1,138 @@
 function xmlChart() {
-    var route = document.getElementById("mySelect").value;
+    const route = document.getElementById("mySelect").value;
+    fetchXMLData(route)
+      .then(xmlText => parseXMLData(xmlText))
+      .then(parsedData => {
+        const ctx = document.getElementById('myChartxml');
+        createChart(ctx, parsedData);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  }
 
-    var dynamicColors = function () {
-        var r = Math.floor(Math.random() * 255);
-        var g = Math.floor(Math.random() * 255);
-        var b = Math.floor(Math.random() * 255);
-        return "rgb(" + r + "," + g + "," + b + ")";
+  function jsonChart() {
+    const route = document.getElementById("mySelect").value;
+    fetchJsonData(route)
+      .then(data => {
+        const ctx = document.getElementById('myChart');
+        const chartData = parseJsonData(data);
+        createChart(ctx, chartData);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  }
+  
+  // Fetch XML data from the server
+  async function fetchXMLData(route) {
+    const response = await fetch(`http://localhost:3000/AGD/${route}`, {
+      headers: {
+        'Content-Type': 'application/xml'
+      }
+    });
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+  
+    return await response.text();
+  }
+
+  // Fetch JSON data from the server
+async function fetchJsonData(route) {
+    const response = await fetch(`http://localhost:3000/AGD/${route}`, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+  
+    return await response.json();
+  }
+  
+  // Parse XML data
+  function parseXMLData(xmlText) {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlText, "text/xml");
+    const countryNodes = xmlDoc.getElementsByTagName('Country');
+    const countryNameNodes = xmlDoc.getElementsByTagName('Country_name');
+    const depressionNodes = xmlDoc.getElementsByTagName('Depression');
+    const GDPNodes = xmlDoc.getElementsByTagName('Log_of_GDP_per_capita');
+    const alcoholNodes = xmlDoc.getElementsByTagName('total_litres_of_pure_alcohol');
+  
+    const sites = Array.from(countryNodes).map((node, i) => {
+      return {
+        label: countryNameNodes[i].firstChild.data.toString(),
+        backgroundColor: dynamicColors(),
+        borderColor: "rgb(69,70,72)",
+        borderWidth: 1,
+        hoverBorderWidth: 2,
+        hoverRadius: 5,
+        data: [
+          {
+            x: Number(alcoholNodes[i].firstChild.data),
+            y: Number(GDPNodes[i].firstChild.data),
+            r: 3.2 * depressionNodes[i].firstChild.data
+          }
+        ]
+      };
+    });
+  
+    return { labels: ["countries"], datasets: sites };
+  }
+
+  // Parse JSON data
+function parseJsonData(data) {
+    const sites = data.map(element => {
+      return {
+        label: element.Country.toString(),
+        backgroundColor: dynamicColors(),
+        borderColor: "rgb(69,70,72)",
+        borderWidth: 1,
+        hoverBorderWidth: 2,
+        hoverRadius: 5,
+        data: [{
+          x: Number(element.total_litres_of_pure_alcohol),
+          y: Number(element.Log_of_GDP_per_capita),
+          r: 3.2 * element.depression
+        }]
+      };
+    });
+  
+    return { labels: ["countries"], datasets: sites };
+  }
+  
+  
+  // Create the chart using Chart.js
+  function createChart(ctx, data) {
+    const options = {
+      title: { display: true, text: 'Depression, gdp and alcohol' },
+      scales: {
+        yAxes: [{
+          scaleLabel: { display: true, labelString: "GDP Ranking" },
+          ticks: { beginAtZero: true }
+        }],
+        xAxes: [{
+          scaleLabel: { display: true, labelString: "total consumption of alcohol" },
+          ticks: { beginAtZero: true }
+        }]
+      }
     };
-
-
-
-    var xmlhttp = new XMLHttpRequest();
-   
-
-    xmlhttp.onreadystatechange = function () {
-        if (this.status == 200) {
-            var myArr = this.responseText;
-            console.log(myArr);
-            parser = new DOMParser();
-            xmlDoc = parser.parseFromString(myArr, "text/xml");
-            var index;
-
-            var Countries_nodes = xmlDoc.getElementsByTagName('Countries');
-            var Country_node = xmlDoc.getElementsByTagName('Country');
-            var Country_name_node = xmlDoc.getElementsByTagName('Country_name');
-            var depression_nodes = xmlDoc.getElementsByTagName('Depression');
-            var GDP_nodes = xmlDoc.getElementsByTagName('Log_of_GDP_per_capita');
-            var alcohol_nodes = xmlDoc.getElementsByTagName('total_litres_of_pure_alcohol');
-
-            var ctx = document.getElementById('myChartxml');
-            var sites = [];
-
-
-            for (i = 0; i < Country_node.length; i++) {
-                console.log(Countries_nodes);
-                var site = {
-
-                    label: Country_name_node[i].firstChild.data.toString(),
-                    backgroundColor: dynamicColors(),
-                    borderColor: "rgb(69,70,72)",
-                    borderWidth: 1,
-                    hoverBorderWidth: 2,
-                    hoverRadius: 5,
-                    data: [
-                        {
-                            x: Number(alcohol_nodes[i].firstChild.data),
-                            y: Number(GDP_nodes[i].firstChild.data),
-                            r: 3.2 * depression_nodes[i].firstChild.data
-                        }
-                    ]
-                };
-                sites.push(site);
-            }
-
-            var data = { labels: ["countries"], datasets: sites };
-            var options = {
-                title: { display: true, text: 'Depression, gdp and alcohol' },
-                scales: {
-                    yAxes:
-                        [
-                            {
-                                scaleLabel: { display: true, labelString: "GDP Ranking" },
-                                ticks: { beginAtZero: true }
-                            }
-                        ],
-                    xAxes:
-                        [
-                            {
-                                scaleLabel: { display: true, labelString: "total consumption of alcohol" },
-                                ticks: { beginAtZero: true }
-                            }
-                        ]
-                }
-            };
-            new Chart(ctx, { type: "bubble", data: data, options: options });
-        }
-
-
-    };
-
-    xmlhttp.open('GET', 'http://localhost:3000/AGD/' + route, true);
-    xmlhttp.setRequestHeader("Content-Type", "application/xml");
-    xmlhttp.send();
-}
+  
+    new Chart(ctx, { type: "bubble", data, options });
+  }
+  
+  // Generate random RGB color
+  const dynamicColors = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r},${g},${b})`;
+  };
+  
+  xmlChart();
+  jsonChart();
